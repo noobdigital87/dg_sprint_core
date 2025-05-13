0d468585dbee1c71ec3ac3b78d8f67b26d603eb7
DiGiTaLnOoB <digitalnoob@noreply.codeberg.org>
2025-05-13 20:36:41 +0000

    init.lua bijwerken

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local mod_name = core.get_current_modname()
 
 dg_sprint_core = {}
 
-dofile(core.get_modpath(mod_name) .. "/register_server_step.lua")
+dofile(core.get_modpath(mod_name) .. "/steps/register_server_step.lua")
 dofile(core.get_modpath(mod_name) .. "/steps/keyboard.lua")
 dofile(core.get_modpath(mod_name) .. "/steps/sprint.lua")
 dofile(core.get_modpath(mod_name) .. "/modules/prevent_sprint.lua")
