a21958b70adebeae45db9dcdd43ab55bd2cd1304
noobdigital87 <noobdigital87@gmail.com>
2025-05-20 13:59:02 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@ local mod_name = core.get_current_modname()
 
 dg_sprint_core = {}
 dg_sprint_core.tools = {}
-dofile(core.get_modpath(mod_name) .. "/lib.lua")
---dofile(core.get_modpath(mod_name) .. "/steps/register_server_step.lua")
+--dofile(core.get_modpath(mod_name) .. "/lib.lua")
+dofile(core.get_modpath(mod_name) .. "/steps/register_server_step.lua")
 dofile(core.get_modpath(mod_name) .. "/steps/keyboard.lua")
 dofile(core.get_modpath(mod_name) .. "/steps/sprint.lua")
 dofile(core.get_modpath(mod_name) .. "/steps/should_drain.lua")
