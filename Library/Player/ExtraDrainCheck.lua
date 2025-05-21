bea091dcf981d9efc4f7739a4f3d2b5776ddd167
noobdigital87 <noobdigital87@gmail.com>
2025-05-21 09:04:10 +0200

    Update ExtraDrainCheck.lua

--- Library/Player/ExtraDrainCheck.lua
+++ Library/Player/ExtraDrainCheck.lua
@@ -1,14 +1,10 @@
-dg_sprint_core.ExtraDrainCheck = function(player)
-
+dg_sprint_core.ExtraDrainCheck = function(player)	
         if not dg_sprint_core.IsMoving(player) then
 		should_still_drain = false
 	elseif
-        	local def = dg_lib.getNodeDefinition(player, {x = pos.x, y = pos.y - 1, z = pos.z})
-        	if def then
-			if not def.walkable then
-            			should_still_drain = false
-			end
-        	end
+		local pos = player:get_pos()
+   		if not dg_sprint_core.IsNodeWalkable(pos) then
+		end
 	end
 	return should_still_drain
 end
