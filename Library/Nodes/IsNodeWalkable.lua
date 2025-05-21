ec9889615862dcf32db30f48a420956d6f54e90f
noobdigital87 <noobdigital87@gmail.com>
2025-05-21 08:36:48 +0200

    Update IsNodeWalkable.lua

--- Library/Nodes/IsNodeWalkable.lua
+++ Library/Nodes/IsNodeWalkable.lua
@@ -1,1 +1,7 @@
-
+dg_sprint_core.IsNodeWalkable = function(player, altPos)
+	local def = dg_sprint_core.getNodeDefinition(player, altPos)
+	if def and def.walkable then
+		return true
+	end
+	return false
+end
