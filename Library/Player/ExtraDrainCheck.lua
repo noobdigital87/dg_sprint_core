ee3686ce41e8e6341abea24d21a7152d54cbebcf
noobdigital87 <noobdigital87@gmail.com>
2025-05-21 08:35:00 +0200

    Update ExtraDrainCheck.lua

--- Library/Player/ExtraDrainCheck.lua
+++ Library/Player/ExtraDrainCheck.lua
@@ -1,5 +1,5 @@
 dg_sprint_core.ExtraDrainCheck = function(player)
-	p_pos = player:get_pos()
+	local pos = player:get_pos()
 	local controls = player:get_player_control()
         local is_moving = controls.up or controls.down or controls.left or controls.right
         local velocity = player:get_velocity()  
@@ -10,6 +10,14 @@ dg_sprint_core.ExtraDrainCheck = function(player)
         if not (is_moving and has_velocity) then
 		should_still_drain = false
 	elseif then
+        	local def = dg_lib.getNodeDefinition(player, {x = pos.x, y = pos.y - 1, z = pos.z})
+        	if def then
+			if def.walkable then
+				player_data[name].on_ground = true
+        		elseif not def.walkable and player_data[name].on_ground then
+            			player_data[name].on_ground = false
+			end
+        	end
 	end
 	return should_still_drain
 end
