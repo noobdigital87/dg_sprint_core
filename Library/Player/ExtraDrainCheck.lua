e6f24e2a028e2854bca83ee4ca9f9d5b3b233f3a
noobdigital87 <noobdigital87@gmail.com>
2025-05-21 08:15:00 +0200

    Update ExtraDrainCheck.lua

--- Library/Player/ExtraDrainCheck.lua
+++ Library/Player/ExtraDrainCheck.lua
@@ -1,3 +1,13 @@
 dg_sprint_core.ExtraDrainCheck = function(player)
-	
+	p_pos = player:get_pos()
+	local controls = player:get_player_control()
+        local is_moving = controls.up or controls.down or controls.left or controls.right
+        local velocity = player:get_velocity()  
+        velocity.y = 0
+	local horizontal_speed = vector.length(velocity)
+	local should_still_drain = true
+        local has_velocity = horizontal_speed > 0.05
+        	if not (is_moving and has_velocity) then
+			should_still_drain = false
+		end
 end
