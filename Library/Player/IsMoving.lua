9f5b6f39a02d4897222fd45854135cc01e5326ec
noobdigital87 <noobdigital87@gmail.com>
2025-05-24 14:06:33 +0200

    Update IsMoving.lua

--- Library/Player/IsMoving.lua
+++ Library/Player/IsMoving.lua
@@ -1,18 +1,12 @@
 dg_sprint_core.IsMoving = function(player)
 	local p_pos = player:get_pos()
 	local controls = player:get_player_control()
-
         local is_moving = controls.up or controls.down or controls.left or controls.right
-
         local velocity = player:get_velocity()  
-
         velocity.y = 0
-
         local horizontal_speed = vector.length(velocity)
 	local has_velocity = horizontal_speed > 0.05
-
 	local is_moving = true
-
         if not (is_moving and has_velocity) then
 		is_moving = false
 	end
