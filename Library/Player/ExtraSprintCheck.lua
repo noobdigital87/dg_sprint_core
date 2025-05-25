01dceece5310c9b23fb83367c33e7dd96f3e4af9
noobdigital87 <noobdigital87@gmail.com>
2025-05-25 06:04:37 +0200

    Update ExtraSprintCheck.lua

--- Library/Player/ExtraSprintCheck.lua
+++ Library/Player/ExtraSprintCheck.lua
@@ -1,6 +1,6 @@
 dg_sprint_core.ExtraSprintCheck = function(player)
 	local should_still_sprint = true
-	if not dg_sprint_core.IsMoving(player) or not player:get_attach() then
+	if not dg_sprint_core.IsMoving(player) or player:get_attach() then
 		should_still_sprint = false
 	end
 	return should_still_sprint
