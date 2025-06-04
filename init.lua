1a0488cfddd16ce7a7b5a2156fc6ab5c3eb0d4c3
noobdigital87 <noobdigital87@gmail.com>
2025-06-04 22:33:33 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -290,7 +290,7 @@ end
 --[[-----------------------------------------------------------------------------------------------------------
 	API [API_NR = 207]
 ]]
-local get_physics_def = function(player)
+api.get_physics_def = function(player)
 	local def = summed_mod_defs
 	local returned_def = {}
 	local name = player:get_player_name()
